b7c2ceba2c8d75a465f8537c80aa44726b26e0e6
Hong Rui <hongruiseth.teo@gmail.com>
2023-07-24 22:11:09 +0800

    Added quiz 5 questions

---
+++ questions/btt5.js
@@ -0,0 +1,418 @@
+const quiz5_questions = [
+    {
+        question: "This sign means",
+        image: "https://res.cloudinary.com/dzh7rqxop/image/upload/v1690207485/btt_test_5/Screenshot_2023-07-24_at_10.02.07_PM_hhftm2.png",
+        answers: [
+          { text: "You must stop for children to cross the road.", correct: true },
+          { text: "Children must not cross the road.", correct: false },
+          { text: "You can proceed as only children have to stop.", correct: false }
+        ]
+      },
+      {
+        question: "This sign means",
+        image: "https://res.cloudinary.com/dzh7rqxop/image/upload/v1690207485/btt_test_5/Screenshot_2023-07-24_at_10.02.15_PM_sfxgja.png",
+        answers: [
+          { text: "You must give way to pedestrians only.", correct: false },
+          { text: "Pedestrians must give way to traffic at the junction.", correct: false },
+          { text: "You must give way to traffic on the major road.", correct: true }
+        ]
+      },
+      {
+        question: "This sign means 'No Entry' for vehicles",
+        image: "https://res.cloudinary.com/dzh7rqxop/image/upload/v1690207485/btt_test_5/Screenshot_2023-07-24_at_10.02.19_PM_yxha8e.png",
+        answers: [
+          { text: "Higher than 2.3 metres.", correct: false },
+          { text: "Longer than 2.3 metres.", correct: false },
+          { text: "Wider than 2.3 metres.", correct: true }
+        ]
+      },
+      {
+        question: "What does this sign mean?",
+        image: "https://res.cloudinary.com/dzh7rqxop/image/upload/v1690207484/btt_test_5/Screenshot_2023-07-24_at_10.02.24_PM_bhh0dz.png",
+        answers: [
+          { text: "Bend to the right.", correct: false },
+          { text: "No right turn.", correct: true },
+          { text: "Road on the right closed.", correct: false }
+        ]
+      },
+      {
+        question: "When you see this sign, you should",
+        image: "https://res.cloudinary.com/dzh7rqxop/image/upload/v1690207485/btt_test_5/Screenshot_2023-07-24_at_10.02.29_PM_d8fq6n.png",
+        answers: [
+          { text: "Stop and give way to traffic from the right and left.", correct: false },
+          { text: "Not slow down as you have the right of way.", correct: false },
+          { text: "Slow down and beware of traffic from the left.", correct: true }
+        ]
+      },
+      {
+        question: "When you see this sign, you should",
+        image: "https://res.cloudinary.com/dzh7rqxop/image/upload/v1690207485/btt_test_5/Screenshot_2023-07-24_at_10.02.33_PM_ul5kmt.png",
+        answers: [
+          { text: "Not slow down as you have the right of way.", correct: false },
+          { text: "Slow down and beware of traffic from the right.", correct: true },
+          { text: "Slow down and give way to traffic from the right and left.", correct: false }
+        ]
+      },
+      {
+        question: "If you need to display the 'vehicle breakdown', it should be placed",
+        image: "https://res.cloudinary.com/dzh7rqxop/image/upload/v1690207485/btt_test_5/Screenshot_2023-07-24_at_10.02.39_PM_kzrd8i.png",
+        answers: [
+          { text: "Just behind the vehicle.", correct: false },
+          { text: "At least 10 metres behind your vehicle.", correct: false },
+          { text: "At least 20 metres behind your vehicle.", correct: true }
+        ]
+      },
+      {
+        question: "When you approached this warning sign, you should",
+        image: "https://res.cloudinary.com/dzh7rqxop/image/upload/v1690207484/btt_test_5/Screenshot_2023-07-24_at_10.02.44_PM_etjgb9.png",
+        answers: [
+          { text: "Continue at the same speed.", correct: false },
+          { text: "Slow down and be prepared to stop.", correct: true },
+          { text: "Increase speed to beat the lights.", correct: false }
+        ]
+      },
+      {
+        question: "What does this sign means?",
+        image: "https://res.cloudinary.com/dzh7rqxop/image/upload/v1690207484/btt_test_5/Screenshot_2023-07-24_at_10.02.50_PM_rmrmte.png",
+        answers: [
+          { text: "Parking lot for police vehicle only.", correct: false },
+          { text: "Parking place for all vehicles.", correct: true },
+          { text: "Parking place for season holders only.", correct: false }
+        ]
+      },
+      {
+        question: "When you see this traffic sign, you should",
+        image: "https://res.cloudinary.com/dzh7rqxop/image/upload/v1690207484/btt_test_5/Screenshot_2023-07-24_at_10.02.54_PM_chptmy.png",
+        answers: [
+          { text: "Drive on the road shoulder.", correct: false },
+          { text: "Overtake vehicles on the left.", correct: false },
+          { text: "Use the left lane of the road.", correct: true }
+        ]
+      },
+      {
+        question: "What does this traffic sign mean?",
+        image: "https://res.cloudinary.com/dzh7rqxop/image/upload/v1690207484/btt_test_5/Screenshot_2023-07-24_at_10.02.58_PM_wnbcxu.png",
+        answers: [
+          { text: "Fire engines are prohibited in the tunnel.", correct: false },
+          { text: "Vehicle carrying dangerous goods are not allowed to enter.", correct: true },
+          { text: "Lane closed - vehicle on fire in the tunnel.", correct: false }
+        ]
+      },
+      {
+        question: "When you see this sign, you should",
+        image: "https://res.cloudinary.com/dzh7rqxop/image/upload/v1690207484/btt_test_5/Screenshot_2023-07-24_at_10.03.04_PM_rlfsoe.png",
+        answers: [
+          { text: "Keep a lookout as there is a school for the blind ahead.", correct: false },
+          { text: "Slow down and beware of elderly people crossing the road.", correct: true },
+          { text: "Stop the car and help the handicapped person to cross the road.", correct: false }
+        ]
+      },
+      {
+        question: "The zig-zag lines on the road indicate that",
+        image: "https://res.cloudinary.com/dzh7rqxop/image/upload/v1690207484/btt_test_5/Screenshot_2023-07-24_at_10.03.11_PM_cxaz35.png",
+        answers: [
+          { text: "The road is not even.", correct: false },
+          { text: "The road is slippery.", correct: false },
+          { text: "There is a zebra crossing ahead.", correct: true }
+        ]
+      },
+      {
+        question: "The Parallel continuous vellow lines as shown in the diagram means",
+        image: "https://res.cloudinary.com/dzh7rqxop/image/upload/v1690207484/btt_test_5/Screenshot_2023-07-24_at_10.03.17_PM_x9jx3c.png",
+        answers: [
+          { text: "No parking on that side of the road.", correct: true },
+          { text: "No parking on the opposite side of the road.", correct: false },
+          { text: "No overtaking allowed on this side of the road.", correct: false }
+        ]
+      },
+      {
+        question: "When the traffic light signal turns 'green', you should check",
+        image: "",
+        answers: [
+          { text: "For oncoming traffic before moving off.", correct: false },
+          { text: "For oncoming traffic and traffic from the right and left before moving off.", correct: true },
+          { text: "The rear view mirror before moving off.", correct: false }
+        ]
+      },
+      {
+        question: "When the traffic lights signal turns amber and you have already passed the stop line you should,",
+        image: "",
+        answers: [
+          { text: "Stop and reverse back to the stop line.", correct: false },
+          { text: "Continue to drive through the junction.", correct: true },
+          { text: "Stop immediately.", correct: false }
+        ]
+      },
+      {
+        question: "When the traffic lights signal turns amber, you should",
+        image: "",
+        answers: [
+          { text: "Accelerate and cross the junction.", correct: false },
+          { text: "Slow down and be ready to stop.", correct: true },
+          { text: "Proceed with caution as the traffic light signals are out of order.", correct: false }
+        ]
+      },
+      {
+        question: "This policeman is giving signal to vehicles approaching",
+        image: "https://res.cloudinary.com/dzh7rqxop/image/upload/v1690207483/btt_test_5/Screenshot_2023-07-24_at_10.03.25_PM_adycig.png",
+        answers: [
+          { text: "From front and behind to go.", correct: false },
+          { text: "From the right may proceed.", correct: true },
+          { text: "From the left may proceed.", correct: false }
+        ]
+      },
+      {
+        question: "When you see a police officer giving this hand signal at a junction, you should",
+        image: "https://res.cloudinary.com/dzh7rqxop/image/upload/v1690207483/btt_test_5/Screenshot_2023-07-24_at_10.03.30_PM_p2pdw1.png",
+        answers: [
+          { text: "Turn left only.", correct: false },
+          { text: "Turn right only.", correct: false },
+          { text: "Stop at the stop line.", correct: true }
+        ]
+      },
+      {
+        question: "When being overtaken, you should",
+        image: "",
+        answers: [
+          { text: "Speed up.", correct: false },
+          { text: "Maintain your speed.", correct: false },
+          { text: "Slow down.", correct: true }
+        ]
+      },
+      {
+        question: "When the motorcycle you are overtaking suddenly increases speed, you should",
+        image: "",
+        answers: [
+          { text: "Accelerate more to overtake it quickly.", correct: false },
+          { text: "Slow down and abort overtaking it.", correct: true },
+          { text: "Sound your horn to warn the driver.", correct: false }
+        ]
+      },
+      {
+        question: "Which car should give way to Car A?",
+        image: "https://res.cloudinary.com/dzh7rqxop/image/upload/v1690207483/btt_test_5/Screenshot_2023-07-24_at_10.03.35_PM_su9kjh.png",
+        answers: [
+          { text: "Car C.", correct: false },
+          { text: "Car B.", correct: true },
+          { text: "Cars B & C.", correct: false }
+        ]
+      },
+      {
+        question: "Which car should give way?",
+        image: "https://res.cloudinary.com/dzh7rqxop/image/upload/v1690207483/btt_test_5/Screenshot_2023-07-24_at_10.03.40_PM_pf7j14.png",
+        answers: [
+          { text: "Car 'A' should give way to Car 'B'.", correct: true },
+          { text: "Car 'B' should speed up to allow Car 'A' to merge.", correct: false },
+          { text: "Car 'B' should stop to allow Car 'A' to merge smoothly.", correct: false }
+        ]
+      },
+      {
+        question: "When driving pass pedestrians or cyclists, you should",
+        image: "",
+        answers: [
+          { text: "Drive at normal speed, close to them.", correct: false },
+          { text: "Slow down and always give ample side clearance.", correct: true },
+          { text: "Drive quickly past them.", correct: false }
+        ]
+      },
+      {
+        question: "When following a bus that pulls up at a bus stop, you should",
+        image: "",
+        answers: [
+          { text: "Accelerate past the bus quickly.", correct: false },
+          { text: "Slow down and watch carefully for pedestrians.", correct: true },
+          { text: "Sound the horn and overtake the bus.", correct: false }
+        ]
+      },
+      {
+        question: "When the 'Red man' signal is on and pedestrians are crossing, you should",
+        image: "",
+        answers: [
+          { text: "Horn the pedestrians to demand the right of way.", correct: true },
+          { text: "Stop and allow pedestrians to cross.", correct: false },
+          { text: "Proceed behind the pedestrians.", correct: false }
+        ]
+      },
+      {
+        question: "When you see children standing by the side of the road, you should",
+        image: "",
+        answers: [
+          { text: "Maintain your normal speed.", correct: false },
+          { text: "Slow down and be prepared to stop if necessary.", correct: true },
+          { text: "Speed pass them.", correct: false }
+        ]
+      },
+      {
+        question: "In which of these situations should you avoid overtaking?",
+        image: "",
+        answers: [
+          { text: "Approaching a dip in the road.", correct: true },
+          { text: "On a 50 km/h road.", correct: false },
+          { text: "In a one-way street.", correct: false }
+        ]
+      },
+      {
+        question: "What should you use the horn for?",
+        image: "",
+        answers: [
+          { text: "To signal your annoyance.", correct: false },
+          { text: "To greet other road users.", correct: false },
+          { text: "To warn others of danger.", correct: true }
+        ]
+      },
+      {
+        question: "If you intend to drive after attending a function, you should",
+        image: "",
+        answers: [
+          { text: "Avoid mixing your alcoholic drinks.", correct: false },
+          { text: "Avoid drinking alcohol.", correct: true },
+          { text: "Drink coffee before drinking alcohol.", correct: false }
+        ]
+      },
+      {
+        question: "The penalty for a repeat drink driving offender is",
+        image: "",
+        answers: [
+          { text: "Fine, disqualification, and a mandatory jail sentence.", correct: true },
+          { text: "Disqualification only.", correct: false },
+          { text: "Fine and disqualification only.", correct: false }
+        ]
+      },
+      {
+        question: "Drink driving can contribute to accidents because",
+        image: "",
+        answers: [
+          { text: "Drivers' judgment is impaired.", correct: true },
+          { text: "Drivers take a shorter time to react.", correct: false },
+          { text: "Drivers' vision is improved.", correct: false }
+        ]
+      },
+      {
+        question: "In the situation shown, the long vehicle is about to turn left, and the car is going straight. The car should",
+        image: "https://res.cloudinary.com/dzh7rqxop/image/upload/v1690207483/btt_test_5/Screenshot_2023-07-24_at_10.03.49_PM_uyikks.png",
+        answers: [
+          { text: "Slow down and stop if necessary.", correct: true },
+          { text: "Maintain its speed and position.", correct: false },
+          { text: "Proceed quickly to pass the junction.", correct: false }
+        ]
+      },
+      {
+        question: "Car A intends to pass Car B near a side road on the left, Car A should",
+        image: "https://res.cloudinary.com/dzh7rqxop/image/upload/v1690207483/btt_test_5/Screenshot_2023-07-24_at_10.03.54_PM_bhvi2q.png",
+        answers: [
+          { text: "Drive faster to pass the junction.", correct: false },
+          { text: "Pass Car B on the right side.", correct: false },
+          { text: "Pass Car B only after the junction.", correct: true }
+        ]
+      },
+      {
+        question: "Before you make a U-turn on the road, you should check your blind spot by",
+        image: "",
+        answers: [
+          { text: "Looking at the rearview mirror.", correct: false },
+          { text: "Looking over your shoulder.", correct: true },
+          { text: "Looking at the side mirrors.", correct: false }
+        ]
+      },
+      {
+        question: "The 'two-second rule' is used in normal driving situations at",
+        image: "",
+        answers: [
+          { text: "Certain speed only.", correct: false },
+          { text: "All speeds.", correct: true },
+          { text: "Fixed speed only.", correct: false }
+        ]
+      },
+      {
+        question: "In order to avoid an accident with the vehicle in front while driving under normal conditions, you should use the",
+        image: "",
+        answers: [
+          { text: "Two-second rule.", correct: true },
+          { text: "Four-second rule.", correct: false },
+          { text: "Two-meter rule.", correct: false }
+        ]
+      },
+      {
+        question: "When being followed by a police vehicle with its siren on, you should",
+        image: "",
+        answers: [
+          { text: "Accelerate quickly to get away from it.", correct: false },
+          { text: "Pull over safely, as soon as possible to let it pass.", correct: true },
+          { text: "Brake quickly and stop to let it pass.", correct: false }
+        ]
+      },
+      {
+        question: "When approaching a road that is flooded but passable to vehicles, you should",
+        image: "",
+        answers: [
+          { text: "Engage a higher gear to go through the flood.", correct: false },
+          { text: "Engage a lower gear to go through the flood.", correct: true },
+          { text: "Drive through as fast as possible.", correct: false }
+        ]
+      },
+      {
+        question: "When driving along a two-way street, you should",
+        image: "",
+        answers: [
+          { text: "Always keep near the center line of the road.", correct: false },
+          { text: "Not cross the center line at bends.", correct: true },
+          { text: "Not overtake at all times.", correct: false }
+        ]
+      },
+      {
+        question: "When turning right at a one-way street, you should position your vehicle",
+        image: "",
+        answers: [
+          { text: "In the right-hand lane.", correct: true },
+          { text: "Just left of the center of the road.", correct: false },
+          { text: "In any lane depending on the traffic conditions.", correct: false }
+        ]
+      },
+      {
+        question: "When driving at night, dip your headlights",
+        image: "",
+        answers: [
+          { text: "To light up the road immediately in front of you.", correct: false },
+          { text: "So as not to dazzle drivers of oncoming vehicles.", correct: true },
+          { text: "To prolong the life of the car battery.", correct: false }
+        ]
+      },
+      {
+        question: "When driving at night, you should use",
+        image: "",
+        answers: [
+          { text: "Dipped headlights.", correct: true },
+          { text: "High beam.", correct: false },
+          { text: "Side lights only.", correct: false }
+        ]
+      },
+      {
+        question: "When should you use the right outermost lane of a carriageway?",
+        image: "",
+        answers: [
+          { text: "When driving at the maximum speed.", correct: false },
+          { text: "When overtaking.", correct: true },
+          { text: "When all other lanes are occupied.", correct: false }
+        ]
+      },
+      {
+        question: "During bus lane prohibited hours, a motorist is allowed to use the bus lane when",
+        image: "",
+        answers: [
+          { text: "It is empty.", correct: false },
+          { text: "The other lanes are congested.", correct: false },
+          { text: "The driver is trying to avoid a collision.", correct: true }
+        ]
+      },
+      {
+        question: "A child is required to use a child restraint if the child is",
+        image: "",
+        answers: [
+          { text: "7 years old.", correct: true },
+          { text: "9 years old.", correct: false },
+          { text: "11 years old.", correct: false }
+        ]
+      },
+]
+
+export {quiz5_questions};
